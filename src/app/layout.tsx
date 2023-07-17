eda7255cbb69da62434610cb92ef0003ae9b6e30
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-17 17:47:16 +0200

    add basic layout styling (font, layout)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
-import '@/styles/globals.css'
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+
+import "@/styles/globals.css";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+// Importing the "Inter" font using Next Font Optimization
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    // Using cn utility to merge tailwind classnames and apply the "Inter" font
+    <html lang="en" className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
+      {/* Setting the min-height of the body to 100vh and using the top-padding to offset the navbar height */}
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">{children}</body>
     </html>
-  )
+  );
 }
