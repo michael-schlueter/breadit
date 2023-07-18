67d55a3358cf513d98f87c8efbb23c78841eb0e7
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-18 15:00:59 +0200

    add navbar with styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 import "@/styles/globals.css";
 
@@ -18,9 +19,21 @@ export default function RootLayout({
 }) {
   return (
     // Using cn utility to merge tailwind classnames and apply the "Inter" font
-    <html lang="en" className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
       {/* Setting the min-height of the body to 100vh and using the top-padding to offset the navbar height */}
-      <body className="min-h-screen pt-12 bg-slate-50 antialiased">{children}</body>
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        {/* Create a tailwind container with max-width and center it horizontally */}
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
