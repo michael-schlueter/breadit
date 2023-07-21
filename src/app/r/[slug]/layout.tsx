dbfdf2a1e3252990bfd878e22254e8cd975e14ec
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-21 23:45:19 +0200

    add functionality to join and leave subreddits

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,3 +1,4 @@
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
@@ -84,6 +85,14 @@ const Layout = async ({
                 <div className="flex justify-between gap-x-4 py-3">
                   <p className="text-gray-500">You created this community</p>
                 </div>
+              ) : null}
+
+              {subreddit.creatorId !== session?.user.id ? (
+                <SubscribeLeaveToggle
+                  subredditId={subreddit.id}
+                  subredditName={subreddit.name}
+                  isSubscribed={isSubscribed}
+                />
               ) : null}
             </dl>
           </div>
---
+++ src/components/SubscribeLeaveToggle.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { FC, startTransition } from "react";
+import { Button } from "./ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import { SubscribeToSubredditPayload } from "@/lib/validators/subreddit";
+import axios, { AxiosError } from "axios";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { subscribe } from "diagnostics_channel";
+
+interface SubscribeLeaveToggleProps {
+  subredditId: string;
+  subredditName: string;
+  isSubscribed: boolean;
+}
+
+const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({
+  subredditId,
+  subredditName,
+  isSubscribed,
+}) => {
+  const { loginToast } = useCustomToast();
+  const router = useRouter();
+
+  const { mutate: subscribe, isLoading: isSubLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubredditPayload = {
+        subredditId,
+      };
+
+      const { data } = await axios.post("/api/subreddit/subscibe", payload);
+      return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong, please try again",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh();
+      });
+
+      return toast({
+        title: "Subscribed",
+        description: `You are now subscribed to r/${subredditName}`,
+      });
+    },
+  });
+
+  const { mutate: unsubscribe, isLoading: isUnSubLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubredditPayload = {
+        subredditId,
+      };
+
+      const { data } = await axios.post("/api/subreddit/unsubscribe", payload);
+      return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong, please try again",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh();
+      });
+
+      return toast({
+        title: "Unsubscribed",
+        description: `You are now unsubscribed from r/${subredditName}`,
+      });
+    },
+  });
+
+  return isSubscribed ? (
+    <Button
+      isLoading={isUnSubLoading}
+      onClick={() => unsubscribe()}
+      className="w-full mt-1 mb-4"
+    >
+      Leave Community
+    </Button>
+  ) : (
+    <Button
+      isLoading={isSubLoading}
+      onClick={() => subscribe()}
+      className="w-full mt-1 mb-4"
+    >
+      Join to post
+    </Button>
+  );
+};
+
+export default SubscribeLeaveToggle;
