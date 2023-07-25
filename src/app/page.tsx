b1c2d70ab1d72e97c17dfb18297fc2e6ea769989
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-25 17:14:38 +0200

    add General Feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import GeneralFeed from "@/components/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
+import { getAuthSession } from "@/lib/auth";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* feed */}
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
@@ -31,7 +36,9 @@ export default function Home() {
                 className: "w-full mt-4 mb-6",
               })}
               href="/r/create"
-            >Create Community</Link>
+            >
+              Create Community
+            </Link>
           </div>
         </div>
       </div>
