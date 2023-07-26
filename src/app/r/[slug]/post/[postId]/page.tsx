095dfa6496ad675a496ff79c96991aa7a3c6110f
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-26 14:10:52 +0200

    add PostCommentsSection

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import CommentsSection from "@/components/CommentsSection";
 import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
@@ -72,6 +73,14 @@ const page = async ({ params }: PageProps) => {
           </h1>
 
           <EditorOutput content={post?.content ?? cachedPost.content} />
+
+          <Suspense
+            fallback={
+              <Loader2 className="h-5 w-5 animate-spin text-zinc-500" />
+            }
+          >
+            <CommentsSection />
+          </Suspense>
         </div>
       </div>
     </div>
