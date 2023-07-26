0f4e3c66c4f589340da1071f73f0bb85b4e398b8
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-26 12:52:02 +0200

    add caching logic for page details

---
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { CachedPost } from "@/types/redis";
+import { Post, User, Vote } from "@prisma/client";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  params: {
+    postId: string;
+  };
+}
+
+// For forcing hard reload when visiting post details (to get recent comments)
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+const page = async ({ params }: PageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
+
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between"></div>
+    </div>
+  );
+};
+
+export default page;
