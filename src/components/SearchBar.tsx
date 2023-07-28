ac5a5a8447b864c8fbcc19b738e1feedcda56b78
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-28 10:09:53 +0200

    add searchbar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/Command";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import { Prisma, Subreddit } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { Users } from "lucide-react";
+import debounce from "lodash.debounce";
+
+const SearchBar = () => {
+  const [input, setInput] = useState<string>("");
+  const router = useRouter();
+
+  const {
+    data: queryResults,
+    refetch,
+    isFetched,
+    isFetching,
+  } = useQuery({
+    queryFn: async () => {
+      if (!input) return [];
+      const { data } = await axios.get(`/api/search?q=${input}`);
+      return data as (Subreddit & {
+        _count: Prisma.SubredditCountOutputType;
+      })[];
+    },
+    queryKey: ["search-query"],
+    enabled: false,
+  });
+
+  const request = debounce(() => {
+    refetch();
+  }, 300);
+
+  const debounceRequest = useCallback(() => {
+    request();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+      <CommandInput
+        value={input}
+        isLoading={isFetching}
+        onValueChange={(text) => {
+          setInput(text);
+          // Request is made only after not typing for 300ms instead of for every keystroke (reduces db traffic)
+          debounceRequest();
+        }}
+        className="outline-none border-none focus:border-none focus:outline-none ring-0"
+        placeholder="Search communities..."
+      />
+
+      {input.length > 0 ? (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-none">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                  key={subreddit.id}
+                  value={subreddit.name}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      ) : null}
+    </Command>
+  );
+};
+
+export default SearchBar;
