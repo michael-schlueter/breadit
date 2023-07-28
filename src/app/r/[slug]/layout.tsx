5e3037ca78c34472cb667f84bb7bffa767d2d67e
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-28 11:44:58 +0200

    add back to community/feed button

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,4 +1,5 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import ToFeedButton from "@/components/ToFeedButton";
 import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -57,7 +58,7 @@ const Layout = async ({
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
-        {/* TODO: Button to take us back */}
+        <ToFeedButton />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <div className="flex flex-col col-span-2 space-y-6">{children}</div>
           {/* info sidebar */}
